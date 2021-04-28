db4fbc5adb882c61f115f3a08336310b05067087
Shaharyar Lakhani <shaharyar.lakhani@utexas.edu>
2021-04-27 23:35:13 -0500

    made static tables

--- omdb-nominate/src/NominationsList.js
+++ omdb-nominate/src/NominationsList.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Table } from 'react-bootstrap';
 
 function NominationsList() {
     return (
         <div>
-            This is where nominations should be.
+            <h1>Nominations</h1>
+            <h4>This is where nominations should be.</h4>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Movie Name</th>
+                        <th>Nominated</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>3 Idiots</td>
+                        <td>Yes</td>
+                    </tr>
+                </tbody>
+            </Table>
         </div>
-    )
+    );
 }
 
 export default NominationsList;
--- omdb-nominate/src/ResultsList.js
+++ omdb-nominate/src/ResultsList.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { Table } from 'react-bootstrap';
 
 function ResultsList() {
     return (
         <div>
-            This is where results should be.
+            <h1>Results</h1>
+            <h4>This is where results should be.</h4>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Movie Name</th>
+                        <th>Nominated</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>3 Idiots</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Wolf of Wall Street</td>
+                        <td>No</td>
+                    </tr>
+                    <tr>
+                        <td>Slumdog Millionaire</td>
+                        <td>No</td>
+                    </tr>
+                </tbody>
+            </Table>
         </div>
-    )
+    );
 }
 
 export default ResultsList;
