d28b2ed1f255cd25cbdc538dd3e9ae34a2fbbfd9
Shaharyar Lakhani <shaharyar.lakhani@utexas.edu>
2021-05-02 15:01:07 -0500

    added header and basic searchbar

---
+++ omdb-nominate/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <h1 style={{textAlign: 'center', 
+                backgroundColor: 'black', 
+                color: 'white',
+                padding: '25px'}}>
+                    Pick your OMDB Nominations!
+        </h1>
+    )
+}
+
+export default Header
