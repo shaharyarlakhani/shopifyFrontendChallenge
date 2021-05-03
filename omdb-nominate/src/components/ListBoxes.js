67ef73daaa9c8214c7991756c52f6a455ad43ac8
Shaharyar Lakhani <shaharyar.lakhani@utexas.edu>
2021-05-02 22:45:35 -0500

    created dummy tables

--- omdb-nominate/src/components/ListBoxes.js
+++ omdb-nominate/src/components/ListBoxes.js
@@ -2,8 +2,37 @@ import React from 'react'
 
 function ListBoxes() {
     return (
-        <div>
-            
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+            <table>
+                <tr>
+                    <th>Movie Name</th>
+                    <th>Info</th>
+                    <th>Nominate Status</th>
+                </tr>
+                <tr>
+                    <td>3 Idiots</td>
+                    <td>Great</td>
+                    <td>Nominated</td>
+                </tr>
+                <tr>
+                    <td>Bad Movie</td>
+                    <td>Bad</td>
+                    <td>Nominate?</td>
+                </tr>
+            </table>
+
+            <table>
+                <tr>
+                    <th>Movie Name</th>
+                    <th>Info</th>
+                    <th>Nominate Action</th>
+                </tr>
+                <tr>
+                    <td>3 Idiots</td>
+                    <td>Great</td>
+                    <td>Remove</td>
+                </tr>
+            </table>
         </div>
     )
 }
